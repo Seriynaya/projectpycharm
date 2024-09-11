45e6ecd69ec378e2d3252c44f35aae6002b1ba5b
Nagaeva Elina <05batman@mail.ru>
2024-09-11 21:14:37 +0400

    Добавление модуля decorators, декоратора log, обновление README файла

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -2,8 +2,7 @@ from typing import Any
 
 import pytest
 
-from src.generators import (card_number_generator, filter_by_currency,
-                            transaction_descriptions)
+from src.generators import card_number_generator, filter_by_currency, transaction_descriptions
 
 
 def test_filter_by_currency(transaction_list: Any, usd_transaction: Any) -> Any:
